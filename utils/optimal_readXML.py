96ca2edd1f3c9691c8d85171489379808a52b78d
Behrad Koohy <bk2g18@soton.ac.uk>
2022-01-05 14:12:43 +0000

    added multiprocessing into db operations

--- utils/optimal_readXML.py
+++ utils/optimal_readXML.py
@@ -3,6 +3,8 @@ import sqlite3
 import xml.etree.ElementTree as ET
 import re
 
+import concurrent
+
 from db_config import emission_outputs, metrics, db_name
 
 log_dir = '/home/behradkoohy/sumo_scratchpad/behrad-resco/RESCO/logs/'
@@ -31,6 +33,7 @@ def create_connection(database):
     conn.commit()
     c.close()
 
+mproc = False
 
 def get_cur(database):
     # conn = sqlite3.connect(database)
@@ -50,7 +53,54 @@ for experiment_name in titles:
     experiments[experiment_name] = experiments.get(experiment_name, []) + [x for x in os.listdir(log_dir) if experiment_name in x]
 
 
+def add_to_db(trial, run_name):
+    run_name_split = run_name.split("-")
+    algorithm = run_name_split[0]
+    environment = run_name_split[2]
+    # trial is already computed
+
+    xml_regex = re.compile("tripinfo_.*\.xml")
+    trip_files = list(filter(xml_regex.match, os.listdir(log_dir + run_name)))
+    for trip_file in trip_files:
+        epoch = int(trip_file.replace("tripinfo_", "").replace(".xml", ""))
+        trip_file_path = log_dir + run_name + os.sep + trip_file
+        if not os.path.exists(trip_file_path):
+            print('No ' + trip_file_path)
+            continue
+        try:
+            tree = ET.parse(trip_file_path)
+            root = tree.getroot()
+            number_trips, total = 0, 0.0
+            metric_outs = {}
+            # last departure time would go here
+            for child in root:
+                try:
+                    number_trips += 1
+                    emission = child[0].attrib
+                    # take the emission metrics
+                    for met in emission_outputs:
+                        metric_outs[met] = metric_outs.get(met, 0) + float(emission[met])
+                    # take the performance metrics
+                    for met in metrics:
+                        metric_outs[met] = metric_outs.get(met, 0) + float(child.attrib[met])
+                except Exception as e:
+                    print(e)
+                    break
+            # print(number_trips, metric_outs)
+            for name, total in metric_outs.items():
+                metric_outs[name] = metric_outs[name] / float(number_trips)
+                update_database_values(name, algorithm, environment, trial, epoch, metric_outs[name])
+            # print(number_trips, metric_outs)
+            # get_cur(db_name).commit()
+            conn.commit()
+
+        except ET.ParseError as e:
+            print(e)
+            break
+
 calculated_outputs = {}
+
+executor = concurrent.futures.ProcessPoolExecutor()
 
 # initialise the database
 create_connection(db_name)
@@ -60,49 +110,12 @@ for experiment in experiments:
     unsorted_experiments.sort(key=lambda tup: tup[0])
     for trial, run_name in unsorted_experiments:
         print(run_name)
-        run_name_split = run_name.split("-")
-        algorithm = run_name_split[0]
-        environment = run_name_split[2]
-        # trial is already computed
+        add_to_db(trial, run_name)
+        if mproc:
+            executor.submit(add_to_db(trial, run_name))
+        else:
+            add_to_db(trial, run_name)
 
-        xml_regex = re.compile("tripinfo_.*\.xml")
-        trip_files = list(filter(xml_regex.match, os.listdir(log_dir + run_name)))
-        for trip_file in trip_files:
-            epoch = int(trip_file.replace("tripinfo_", "").replace(".xml", ""))
-            trip_file_path = log_dir + run_name + os.sep + trip_file
-            if not os.path.exists(trip_file_path):
-                print('No ' + trip_file_path)
-                continue
-            try:
-                tree = ET.parse(trip_file_path)
-                root = tree.getroot()
-                number_trips, total = 0, 0.0
-                metric_outs = {}
-                # last departure time would go here
-                for child in root:
-                    try:
-                        number_trips += 1
-                        emission = child[0].attrib
-                        # take the emission metrics
-                        for met in emission_outputs:
-                            metric_outs[met] = metric_outs.get(met, 0) + float(emission[met])
-                        # take the performance metrics
-                        for met in metrics:
-                            metric_outs[met] = metric_outs.get(met, 0) + float(child.attrib[met])
-                    except Exception as e:
-                        print(e)
-                        break
-                # print(number_trips, metric_outs)
-                for name, total in metric_outs.items():
-                    metric_outs[name] = metric_outs[name] / float(number_trips)
-                    update_database_values(name, algorithm, environment, trial , epoch, metric_outs[name])
-                # print(number_trips, metric_outs)
-                # get_cur(db_name).commit()
-                conn.commit()
-
-            except ET.ParseError as e:
-                print(e)
-                break
 
     # create_connection("test")
 
