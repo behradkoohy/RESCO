000b51a7995697ca8530a8dda97045ef842dbec2
Behrad Koohy <bk2g18@soton.ac.uk>
2022-01-24 11:26:14 +0000

    girlfriend stole my desk, so i am pushing to git and working from campus

--- agent_config.py
+++ agent_config.py
@@ -10,6 +10,7 @@ from agents.mplight import MPLight
 from agents.fma2c import FMA2C
 from agents.graph import Graph_IDQN
 from agents.graph_rec import Graph_RIDQN
+from agents.colight import Colight
 
 from agents.models.TwoConvLayers import TwoConvLayer
 
@@ -256,5 +257,31 @@ agent_configs = {
         'EPS_DECAY': 220,
         'TARGET_UPDATE': 500,
         'demand_shape': 1
+    },
+    'ADMPLight_WN': {
+        'agent': Colight,
+        'state': states.drq_norm,
+        'reward': rewards.wait_norm,
+        'max_distance': 200,
+        'BATCH_SIZE': 32,
+        'GAMMA': 0.99,
+        'EPS_START': 1.0,
+        'EPS_END': 0.0,
+        'EPS_DECAY': 220,
+        'TARGET_UPDATE': 500,
+        'demand_shape': 1
+    },
+
+    'COL': {
+        'agent': Colight,
+        'state': states.graph_pooled,
+        'reward': rewards.wait_norm,
+        'max_distance': 200,
+        'BATCH_SIZE': 32,
+        'GAMMA': 0.99,
+        'EPS_START': 1.0,
+        'EPS_END': 0.0,
+        'EPS_DECAY': 220,
+        'TARGET_UPDATE': 500
     },
 }
