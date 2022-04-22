49fad236d8015c96ee29f52aa51528b34caa9bdb
Behrad Koohy <bk2g18@soton.ac.uk>
2022-04-22 10:45:10 +0100

    added changes for iridis

--- rewards.py
+++ rewards.py
@@ -8,6 +8,8 @@ def wait(signals):
     for signal_id in signals:
         total_wait = 0
         for lane in signals[signal_id].lanes:
+            import pdb
+            pdb.set_trace()
             total_wait += signals[signal_id].full_observation[lane]['total_wait']
 
         rewards[signal_id] = -total_wait
@@ -36,6 +38,39 @@ def pressure(signals):
             dwn_signal = signals[signal_id].out_lane_to_signalid[lane]
             if dwn_signal in signals[signal_id].signals:
                 queue_length -= signals[signal_id].signals[dwn_signal].full_observation[lane]['queue']
+
+        rewards[signal_id] = -queue_length
+    return rewards
+
+def queue(signals):
+    rewards = dict()
+    for signal_id in signals:
+        queue_length = 0
+        for lane in signals[signal_id].lanes:
+            queue_length += (signals[signal_id].full_observation[lane]['queue'])
+        rewards[signal_id] = -queue_length
+    return rewards
+
+def queue_sq(signals):
+    rewards = dict()
+    for signal_id in signals:
+        queue_length = 0
+        for lane in signals[signal_id].lanes:
+            queue_length += (signals[signal_id].full_observation[lane]['queue'] ** 2)
+        rewards[signal_id] = -queue_length
+    return rewards
+
+def pressure_sq(signals):
+    rewards = dict()
+    for signal_id in signals:
+        queue_length = 0
+        for lane in signals[signal_id].lanes:
+            queue_length += (signals[signal_id].full_observation[lane]['queue'] ** 2)
+
+        for lane in signals[signal_id].outbound_lanes:
+            dwn_signal = signals[signal_id].out_lane_to_signalid[lane]
+            if dwn_signal in signals[signal_id].signals:
+                queue_length -= (signals[signal_id].signals[dwn_signal].full_observation[lane]['queue'] ** 2)
 
         rewards[signal_id] = -queue_length
     return rewards
