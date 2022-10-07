fdc116f7b4611031618856f14e72c78dbafe0b71
Behrad Koohy <bk2g18@soton.ac.uk>
2022-10-07 11:09:20 +0100

    alright so changes made

--- send_jobs.sh
+++ send_jobs.sh
@@ -1,7 +1,7 @@
-#for AGENT in "IDQN" "MPLight" "MPLightFULL"; do
-#for AGENT in "IDQN" "IDQN_average_speed" "IDQN_average_speed_norm" "IDQN_mwait" "IDQN_wait" "IDQN_wait_norm" "IDQN_pressure" "IDQN_queue" "IDQN_queue_sq" "IDQN_pressure_sq" "IDQN_queue_maxwait" "IDQN_queue_maxwait_neighbourhood"; do
-#for AGENT in "IDQN" "IDQN_mwait" "IDQN_wait" "IDQN_wait_norm" "IDQN_queue" "IDQN_queue_sq" "IDQN_queue_maxwait" "IDQN_queue_maxwait_neighbourhood"; do
-for AGENT in "STOCHASTIC"; do
+#for AGENT in "Rainbow" "MPLight" "MPLightFULL"; do
+for AGENT in "Rainbow" "Rainbow_average_speed" "Rainbow_average_speed_norm" "Rainbow_mwait" "Rainbow_wait" "Rainbow_wait_norm" "Rainbow_pressure" "Rainbow_queue" "Rainbow_queue_sq" "Rainbow_pressure_sq" "Rainbow_queue_maxwait" "Rainbow_queue_maxwait_neighbourhood"; do
+#for AGENT in "Rainbow" "Rainbow_mwait" "Rainbow_wait" "Rainbow_wait_norm" "Rainbow_queue" "Rainbow_queue_sq" "Rainbow_queue_maxwait" "Rainbow_queue_maxwait_neighbourhood"; do
+# for AGENT in "STOCHASTIC"; do
 	for N in {1..20}; do
 		sbatch noalpha_dist_task.slurm $AGENT $N
 	done
