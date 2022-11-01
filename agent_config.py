7ec780b8309ad222911752a0811e07737e96e34c
Behrad Koohy <bk2g18@soton.ac.uk>
2022-11-01 11:21:38 +0000

    added didqn

--- agent_config.py
+++ agent_config.py
@@ -13,7 +13,8 @@ from agents.graph_rec import Graph_RIDQN
 from agents.rainbow import Rainbow
 from agents.srainbow import SRainbow
 from agents.colight import Colight
-from agents.models.TwoConvLayers import TwoConvLayer
+from agents.duel_dqn import DIDQN
+
 
 
 agent_configs = {
@@ -551,6 +552,19 @@ agent_configs = {
         'agent': SRainbow,
         'state': states.drq_norm,
         'reward': rewards.queue_maxwait_neighborhood,
+        'max_distance': 200,
+        'BATCH_SIZE': 32,
+        'GAMMA': 0.99,
+        'EPS_START': 1.0,
+        'EPS_END': 0.0,
+        'EPS_DECAY': 220,
+        'TARGET_UPDATE': 500
+    },
+
+    'DIDQN': {
+        'agent': DIDQN,
+        'state': states.drq_norm,
+        'reward': rewards.wait_norm,
         'max_distance': 200,
         'BATCH_SIZE': 32,
         'GAMMA': 0.99,
